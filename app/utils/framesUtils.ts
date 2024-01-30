74bce2af3628f957c8b2e7591c3a71b53449f6d9
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2024-01-29 22:30:09 -0800

    try again w metadata

---
+++ app/utils/framesUtils.ts
@@ -0,0 +1,24 @@
+type FrameMetadataResponse = {
+    buttons: string[];
+    image: string;
+    post_url: string;
+};
+
+/**
+ * This function generates the metadata for a Farcaster Frame.
+ * @param buttons: An array of button names.
+ * @param image: The image to use for the frame.
+ * @param post_url: The URL to post the frame to.
+ * @returns The metadata for the frame.
+ */
+export const getFrameMetadata = function ({ buttons, image, post_url }: FrameMetadataResponse) {
+    const metadata: Record<string, string> = {
+        'fc:frame': 'vNext',
+    };
+    buttons.forEach((button, index) => {
+        metadata[`fc:frame:button:${index + 1}`] = button;
+    });
+    metadata['fc:frame:image'] = image;
+    metadata['fc:frame:post_url'] = post_url;
+    return metadata;
+};
