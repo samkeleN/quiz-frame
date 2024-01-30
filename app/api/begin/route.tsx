828f40b3c64dd2c0d528a67f57bb2fb9b1f60bea
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2024-01-29 21:35:58 -0800

    add /begin route

---
+++ app/api/begin/route.tsx
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+const PROMPT = 'Begin quiz'
+const COVER_IMAGE = '/titleImage.png'
+
+// show quiz title image
+// show begin button
+// rediect to api/question/1
+
+async function getResponse(): Promise<NextResponse> {
+    return new NextResponse(
+        `<!DOCTYPE html>
+            <html>
+            <head>
+                <meta property="fc:frame" content="vNext" />
+                <meta property="fc:frame:image" content="${COVER_IMAGE}" />
+                <meta property="fc:frame:button:1" content="${PROMPT}" />
+                <meta property="fc:frame:post_url" content="${process.env.DOMAIN}/question/1" />
+            </head>
+            <body>
+                <!-- Your frame content here -->
+            </body>
+            </html>
+            `
+    );
+}
+
+export async function POST(): Promise<Response> {
+    return getResponse();
+}
